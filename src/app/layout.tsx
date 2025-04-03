d90f81fbbac945051ca3c8257581ba6847dd812b
Yan Castro <yancastrohenrique1@gmail.com>
2025-04-03 08:36:23 -0300

    feat: implementa estrutura base do layout e estilos globais

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/presentation/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,8 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-arc-foregroundPrimary font-[family-name:var(--font-geist-sans)`}
       >
+        <Header />
         {children}
       </body>
     </html>
