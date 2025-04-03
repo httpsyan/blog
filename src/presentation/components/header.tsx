3236455f0ddb5547962a3ea7ade8efa6828bbc05
Yan Castro <yancastrohenrique1@gmail.com>
2025-04-03 08:36:42 -0300

    feat: implementa componente Header com posicionamento fixo

---
+++ src/presentation/components/header.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <header
+      className={
+        "sticky top-0 z-50 bg-arc-background-gradient-color1 shadow-sm"
+      }
+    >
+      aaaa
+    </header>
+  );
+};
