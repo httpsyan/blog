eb9a138f00f097ef7d3e31429b5e7545501b37b7
Yan Castro <yancastrohenrique1@gmail.com>
2025-04-03 09:39:10 -0300

    feat: adiciona estrutura de constantes e rotas

---
+++ src/infrastructure/constants/routes/header.ts
@@ -0,0 +1,18 @@
+export const MAIN_NAVIGATION_ITEMS = [
+  { name: "World", href: "/world" },
+  { name: "U.S.", href: "/us" },
+  { name: "Business", href: "/business" },
+  { name: "Politics", href: "/politics" },
+  { name: "Economy", href: "/economy" },
+  { name: "Finance", href: "/finance" },
+  { name: "Health", href: "/health" },
+  { name: "Tech", href: "/tech" },
+  { name: "Sports", href: "/sports" },
+] as const;
+
+export const SECONDARY_NAVIGATION_ITEMS = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Authors", href: "/authors" },
+  { name: "Contact", href: "/contact" },
+] as const;
