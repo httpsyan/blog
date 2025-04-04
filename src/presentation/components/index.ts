e409aad0707d642c02782942735a0af142fb57c6
Yan Castro <yancastrohenrique1@gmail.com>
2025-04-03 22:34:45 -0300

    chore: atualiza exportações dos componentes

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,2 +1,5 @@
 export * from "./header";
 export * from "./hero";
+export * from "./featured-card";
+export * from "./aside-card";
+export * from "./footer";
--- src/presentation/ui/index.ts
+++ src/presentation/ui/index.ts
@@ -2,3 +2,4 @@ export * from "./button";
 export * from "./navigation-menu";
 export * from "./input";
 export * from "./sheet";
+export * from "./separator";
